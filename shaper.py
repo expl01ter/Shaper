21217c32d29af31d97574747ab736d07bd758798
exploiter <110817671+expl01ter@users.noreply.github.com>
2022-08-10 12:41:11 +0400

    Update shaper.py
    Обновлён до версии 1.1

--- shaper.py
+++ shaper.py
@@ -1,27 +1,31 @@
 from turtle import *
-print("           _____________  _________________________________")
-print("           __  ___/__  / / /__    |__  __ \__  ____/__  __ \ ")
-print("           _____ \__  /_/ /__  /| |_  /_/ /_  __/  __  /_/ /")
-print("           ____/ /_  __  / _  ___ |  ____/_  /___  _  _, _/ ")
-print("           /____/ /_/ /_/  /_/  |_/_/     /_____/  /_/ |_| V1.0")
-print(" ")
-s=input("Введите цвет лининий на английском языке: ")
-x=input("Введите цвет заливки на английском языке: ")
-print(" ")
-y=input("Введите толщину линий в пикселях: ")
-n=int(input("Введите количество сторон: "))
-f=int(input("Введите размер длин: "))
-print(" ")
-i=0
-width(y)
-color(s)
-begin_fill()
-while i<n:
-    forward(f)
-    left(360/n)
-    i=i+1
-color(x)
-end_fill()
-print(" ")
-print("Finish!")
-exitonclick()
+from pifiglet import Figlet
+import colorama
+import time
+
+f = Figlet(font='standard')
+
+while True:
+	print(Fore.VIOLET + f.renderText('SHAPER') + Style.RESET_ALL)
+	print("Shaper V1.1")
+	print(" ")
+	s=input("Введите цвет лининий на английском языке: ")
+	x=input("Введите цвет заливки на английском языке: ")
+	print(" ")
+	y=input("Введите толщину линий в пикселях: ")
+	n=int(input("Введите количество сторон: "))
+	f=int(input("Введите размер длин: "))
+	print(" ")
+	i=0
+	width(y)
+	color(s)
+	begin_fill()
+	while i<n:
+		forward(f)
+		left(360/n)
+		i=i+1
+	color(x)
+	end_fill()
+	print(" ")
+	print(Fore.GREEN + "Finish!" + Style.RESET_ALL)
+	exitonclick()
